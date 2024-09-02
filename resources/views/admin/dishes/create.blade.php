87572e72bfae08f20e2a402384457a84da8fed67
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-02 10:11:38 +0200

    Dsihes create

---
+++ resources/views/admin/dishes/create.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<h1 class="text-center py-2">Aggiungi un Nuovo Piatto</h1>
+
+		{{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
+		<div>
+			@if ($errors->any())
+				<div class="alert alert-danger">
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
+		</div>
+
+		<form method="POST" action="{{ route('admin.restaurants.store') }}" enctype="multipart/form-data">
+			@csrf
+
+			<div class="mb-3">
+				<label for="name" class="form-label">Nome del Piatto</label>
+				<input type="text" class="form-control" name="name" value="{{ old('name') }}" min="4" max="255"
+					required>
+				@error('name')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
+
+			<div class="mb-3">
+				<label for="price" class="form-label">Prezzo</label>
+				<input type="text" class="form-control" name="price" value="{{ old('price') }}" required>
+				@error('address')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
+
+			<div class="mb-3">
+				<label for="description" class="form-label">Descrizione</label>
+				<input type="text" class="form-control" name="description" value="{{ old('tax_id') }}" min="4"
+					max="255" required>
+				@error('tax_id')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
+
+			<div class="mb-3">
+				<label for="img" class="form-label"></label>
+				<input type="file" class="form-control" name="img" value="{{ old('img') }}" required>
+				@error('img')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
+
+			<button type="submit" class="btn btn-outline-primary">Invia Nuovo Piatto</button>
+		</form>
+	</div>
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,85 +2,89 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fontawesome 6 cdn -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
-        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
-        crossorigin='anonymous' referrerpolicy='no-referrer' />
+	<!-- Fontawesome 6 cdn -->
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+		integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
+		crossorigin='anonymous' referrerpolicy='no-referrer' />
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+	<!-- Usando Vite -->
+	@vite(['resources/js/app.js'])
 </head>
 
 <body>
-    <div id="app">
+	<div id="app">
 
-        <div class="container-fluid vh-100">
-            <div class="row h-100">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
-                    <div class="position-sticky pt-3">
-                        <ul class="nav flex-column">
+		<div class="container-fluid vh-100">
+			<div class="row h-100">
+				<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
+					<div class="position-sticky pt-3">
+						<ul class="nav flex-column">
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="/">
-                                    <i class="fa-solid fa-home-alt fa-lg fa-fw"></i> Home
-                                </a>
-                            </li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="/">
+									<i class="fa-solid fa-home-alt fa-lg fa-fw"></i> Home
+								</a>
+							</li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.dashboard') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
-                                </a>
-                            </li>
+							<li class="nav-item">
+								<a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
+									href="{{ route('admin.dashboard') }}">
+									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+								</a>
+							</li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white"
-                                    href="{{ route('admin.restaurants.index') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Lista ristorante
-                                </a>
-                            </li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="{{ route('admin.restaurants.index') }}">
+									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Lista ristorante
+								</a>
+							</li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white"
-                                    href="{{ route('admin.restaurants.create') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Crea ristorante
-                                </a>
-                            </li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="{{ route('admin.restaurants.create') }}">
+									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Crea ristorante
+								</a>
+							</li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="{{ route('admin.dishes.create') }}">
+									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Crea piatto
+								</a>
+							</li>
 
-                        </ul>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="{{ route('logout') }}"
+									onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+									<i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
+								</a>
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+									@csrf
+								</form>
+							</li>
 
-                    </div>
-                </nav>
+						</ul>
 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
-                </main>
-            </div>
-        </div>
+					</div>
+				</nav>
 
-    </div>
+				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+					@yield('content')
+				</main>
+			</div>
+		</div>
+
+	</div>
 </body>
 
 </html>
