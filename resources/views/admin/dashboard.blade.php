6de5e603d46069463eeb7d4e60770d3b76833c51
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-02 11:34:15 +0200

    dashboard nome ristorante

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+	<div class="container-fluid mt-4">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+					<div class="card-body">
+						@if (session('status'))
+							<div class="alert alert-success" role="alert">
+								{{ session('status') }}
+							</div>
+						@endif
 
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+						{{ __('You are logged in!') }} <br>
+
+						<h2>Il tuo ristorante :</h2>
+						<h1>{{ Auth::user()->restaurant->name }}</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
