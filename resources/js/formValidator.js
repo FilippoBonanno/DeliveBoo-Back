8204475c9662b55cbd464857620f064e0346be94
MikeMac <micheledong2803@gmail.com>
2024-09-03 17:39:53 +0200

    email validation

--- resources/js/formValidator.js
+++ resources/js/formValidator.js
@@ -1,33 +1,50 @@
-
 let registrati_cta = document.getElementById('registrati_cta');
 
-registrati_cta.addEventListener('click', function(){
+// Caratteri alfanumerici
+let alfanumerici = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+registrati_cta.addEventListener('click', function () {
     let email = document.getElementById('email');
     let password = document.getElementById('password');
     let password_confirm = document.getElementById('password_confirm');
     let form = document.getElementById('user_register_form');
 
-    form.addEventListener('submit',function(event){
-        event.preventDefault();
-        let isValid = false;
-        if(containsSpecificCharacters(email.value,'@')){
-            if(containsSpecificCharacters(email.value,'.')){
-                isValid = true;
-            }
-        }
-    })
+    //validazione mail
+    if(emailChecker(email.value)){
+        console.log(email.value)
+    };
 
 })
 
-function containsSpecificCharacters(str, characters) {
-    for (let i = 0; i < characters.length; i++) {
-        if (str.includes(characters[i])) {
-            return true; // Restituisce true se uno dei caratteri è trovato
-        }
-    }
-    return false; // Restituisce false se nessuno dei caratteri è trovato
-}
+function emailChecker(email) {
 
-function hasMultipleSpaces(str) {
-    return /\s{2,}/.test(str);
+    // Controlla se contiene il . e la @
+    if (!email.includes("@") || !email.includes(".")) {
+        return false;
+    }
+    // Controlla che ci sia un solo "@" e che sia in una posizione valida
+    const atIndex = email.indexOf('@');
+    if(atIndex === -1 || atIndex !== email.lastIndexOf('@')){
+        return false
+    }
+    // Controlla che ci sia un punto dopo "@" e che non sia l'ultimo carattere
+    const dotIndex = email.indexOf(".", atIndex);
+    if (dotIndex === -1 || dotIndex === email.length - 1) {
+        return false;
+    }
+    // Controlla che il primo e l'ultimo carattere siano alfanumerici
+    if(!alfanumerici.includes(email[0]) || !alfanumerici.includes(email[email.length-1])){
+        return false
+    }
+    // Controlla che non ci siano spazi bianchi
+    if(email.includes(' ')){
+        return false
+    }
+    // Controlla che non ci siano ".." o ".@" o "@."
+    if (email.includes("..") || email.includes(".@") || email.includes("@.")) {
+        return false;
+    }
+    // Se tutti i controlli passano, l'email è valida
+    return true
+    
 }
