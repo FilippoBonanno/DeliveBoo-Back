0afea6dee7662047244c53c58e0b483dd8b239bf
damirusso95 <damirusso95@gmail.com>
2024-09-03 14:31:24 +0200

    jQuery and Bootstrap JS

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,10 @@
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+	<!-- jQuery and Bootstrap JS (Before app.js) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 	<!-- Usando Vite -->
 	@vite(['resources/js/app.js'])
 </head>
