fd3175ad6d978454fd6ecde30ba741e5c4607814
MikeMac <micheledong2803@gmail.com>
2024-09-05 14:48:12 +0200

    dishFormValidator fix

--- resources/js/dishFormValidator.js
+++ resources/js/dishFormValidator.js
@@ -4,15 +4,20 @@ let description = document.querySelector('[name=description]');
 
 let form = document.getElementById('storeDish');
 
+let errorClientMessage = document.querySelectorAll('.errorClientMessage');
+
 let errori = false;
 
 let trimmedDescription = '';
 
 console.log('ciao');
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
+
+    resetErrors();
+
     //prezzo
-    if (!price.value.includes(',') ) {
+    if (!priceValidator(price.value)) {
         price.style.border = 'red solid 1px'
         price.insertAdjacentHTML('afterend', `<div class="errorClientMessage text-danger" role="alert">
 										Il campo Prezzo deve avere 2 numeri decimali
@@ -21,12 +26,12 @@ form.addEventListener('submit', function(event){
         errori = true;
     }
 
-    trimmedDescription = description.value.trim();
     //descrizione
+    trimmedDescription = description.value.trim();
     if (trimmedDescription.length < 20) {
         description.style.border = 'red solid 1px'
         description.insertAdjacentHTML('afterend', `<div class="errorClientMessage text-danger" role="alert">
-										Il campo Prezzo deve contenere almeno 20 caratteri
+										Il campo Descrizione deve contenere almeno 20 caratteri
 							</div>`);
         description.value = '';
         errori = true;
@@ -34,10 +39,31 @@ form.addEventListener('submit', function(event){
 
 
     //Se ci sono errori allora non far inviare il form
-    if(errori){
+    if (errori) {
         event.preventDefault();
     }
 })
+function priceValidator(price) {
+
+    // Converti il prezzo in stringa, se non lo è già
+    price = price.toString();
+
+    // Controlla se il prezzo è un numero valido
+    let numericPrice = parseFloat(price);
+
+    // Controlla che il prezzo sia un numero compreso tra 0 e 999.99
+    if (isNaN(numericPrice) || numericPrice < 0 || numericPrice > 999.99) {
+        return false;
+    }
+
+    // Controlla che ci sia esattamente un punto decimale e che ci siano 2 cifre decimali
+    let dotIndex = price.indexOf('.');
+    if (dotIndex === -1 || price.length - dotIndex !== 3) {
+        return false;
+    }
+
+    return true;
+}
 
 function resetErrors() {
     errorClientMessage = document.querySelectorAll('.errorClientMessage');
