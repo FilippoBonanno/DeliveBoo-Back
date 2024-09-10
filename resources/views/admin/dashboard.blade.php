e3b2b1511404fd0671cf8ff73be0f4639283f3de
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-10 15:36:16 +0200

    fix dasboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,14 +21,14 @@
 							<h1>{{ Auth::user()->restaurant->name }}</h1>
 							<div class="card p-0 h-100">
 								@if (Str::startsWith($restaurant->img, 'http'))
-    <div class="col-4">
-        <img src="{{ $restaurant->img }}" alt="immagine-ristorante" class="w-100 rounded-start">
-    </div>
-@else
-    <div class="col-4">
-        <img src="{{ asset('storage/' . $restaurant->img) }}" class="w-100 rounded-start" alt="immagine-ristorante">
-    </div>
-@endif
+									<div class="col-4">
+										<img src="{{ $restaurant->img }}" alt="immagine-ristorante" class="w-100 rounded-start">
+									</div>
+								@else
+									<div class="col-4">
+										<img src="{{ asset('storage/' . $restaurant->img) }}" class="w-100 rounded-start" alt="immagine-ristorante">
+									</div>
+								@endif
 
 								<div class="row g-0 m-0">
 									<div class="col-12">
@@ -37,8 +37,8 @@
 											<p class="card-text"><b>Indirizzo Ristorante:</b> {{ $restaurant->address }}</p>
 											<p class="card-text"><b>Categorie:</b>
 												@foreach ($restaurant->categories as $category)
-													{{ $category->name }} 
-													@if($loop->index+1 != count($restaurant->categories))
+													{{ $category->name }}
+													@if ($loop->index + 1 != count($restaurant->categories))
 														-
 													@endif
 												@endforeach
@@ -47,7 +47,7 @@
 									</div>
 								</div>
 							</div>
-						{{-- @else
+							{{-- @else
 							<h2>
 								<a href="{{ route('admin.restaurants.create') }}">
 									Crea ristorante!
@@ -56,87 +56,89 @@
 						@endif
 
 						{{-- lista piatti --}}
-						@if($dishes !== [])
-						<div class="text-center px-2 row m-0">
-							@foreach ($dishes as $dish)
-								<div class="col-4 mt-3 border border-primary">
-									<h4>Name</h4>
-									<div>
-										<a href="{{ route('admin.dishes.show', $dish) }}">{{ $dish->name }}</a>
-									</div>
-									<h4>Price</h4>
-									<div>
-										{{ $dish->price }}
-									</div>
-									<h4>Img</h4>
-									@if (Str::startsWith($dish->img, 'http'))
-										<div class="w-100">
-											<img src="{{ $dish->img }}" alt="" class="w-50">
-										</div>
-									@else
+						@if (!empty($dishes))
+							<div class="text-center px-2 row m-0">
+								@foreach ($dishes as $dish)
+									<div class="col-4 mt-3 border border-primary">
+										<h4>Name</h4>
 										<div>
-											<img src="{{ asset('storage/' . $dish->img) }}" alt="img" class="w-50">
+											<a href="{{ route('admin.dishes.show', $dish) }}">{{ $dish->name }}</a>
 										</div>
-									@endif
-									<h4>Description</h4>
-									<div>
-										{{ $dish->description }}
+										<h4>Price</h4>
+										<div>
+											{{ $dish->price }}
+										</div>
+										<h4>Img</h4>
+										@if (Str::startsWith($dish->img, 'http'))
+											<div class="w-100">
+												<img src="{{ $dish->img }}" alt="" class="w-50">
+											</div>
+										@else
+											<div>
+												<img src="{{ asset('storage/' . $dish->img) }}" alt="img" class="w-50">
+											</div>
+										@endif
+										<h4>Description</h4>
+										<div>
+											{{ $dish->description }}
+										</div>
+										<h4>Restaurant</h4>
+										<div>
+											{{ $dish->restaurant->name }}
+										</div>
+										<a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-primary">Edit</a>
+										<button type="button" class="btn btn-outline-danger my-3" data-toggle="modal" data-target="#confirmModal"
+											data-action="{{ route('admin.dishes.destroy', $dish) }}">Elimina Piatto</button>
 									</div>
-									<h4>Restaurant</h4>
-									<div>
-										{{ $dish->restaurant->name }}
-									</div>
-									<a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-primary">Edit</a>
-									<button type="button" class="btn btn-outline-danger my-3" data-toggle="modal" data-target="#confirmModal" data-action="{{ route('admin.dishes.destroy', $dish) }}">Elimina Piatto</button>
-								</div>
-							@endforeach
-						</div>
-					
-						<!-- Modale di conferma -->
-						<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
-							<div class="modal-dialog" role="document">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="confirmModalLabel">Conferma Eliminazione</h5>
-									</div>
-									<div class="modal-body">
-										Sei sicuro di voler eliminare questo piatto?
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
-										<form id="confirmDeleteForm" method="POST" action="">
-											@csrf
-											@method('DELETE')
-											<button type="submit" class="btn btn-danger">Elimina</button>
-										</form>
+								@endforeach
+							</div>
+
+							<!-- Modale di conferma -->
+							<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel"
+								aria-hidden="true">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title" id="confirmModalLabel">Conferma Eliminazione</h5>
+										</div>
+										<div class="modal-body">
+											Sei sicuro di voler eliminare questo piatto?
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+											<form id="confirmDeleteForm" method="POST" action="">
+												@csrf
+												@method('DELETE')
+												<button type="submit" class="btn btn-danger">Elimina</button>
+											</form>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					
-						<script>
-							document.addEventListener('DOMContentLoaded', function () {
-								// Seleziona tutti i bottoni di eliminazione
-								const deleteButtons = document.querySelectorAll('button[data-action]');
-								
-								deleteButtons.forEach(button => {
-									button.addEventListener('click', function () {
-										// Ottieni l'azione del form dalla proprietà data-action del bottone
-										const action = this.getAttribute('data-action');
-										// Imposta l'azione del form nella modale
-										document.getElementById('confirmDeleteForm').action = action;
-										// Mostra la modale
-										$('#confirmModal').modal('show');
+
+							<script>
+								document.addEventListener('DOMContentLoaded', function() {
+									// Seleziona tutti i bottoni di eliminazione
+									const deleteButtons = document.querySelectorAll('button[data-action]');
+
+									deleteButtons.forEach(button => {
+										button.addEventListener('click', function() {
+											// Ottieni l'azione del form dalla proprietà data-action del bottone
+											const action = this.getAttribute('data-action');
+											// Imposta l'azione del form nella modale
+											document.getElementById('confirmDeleteForm').action = action;
+											// Mostra la modale
+											$('#confirmModal').modal('show');
+										});
+									});
+
+									// Gestisce la chiusura della modale quando clicchi su "Annulla"
+									const cancelButton = document.querySelector('.btn-secondary');
+									cancelButton.addEventListener('click', function() {
+										$('#confirmModal').modal('hide');
 									});
 								});
-					
-								// Gestisce la chiusura della modale quando clicchi su "Annulla"
-								const cancelButton = document.querySelector('.btn-secondary');
-								cancelButton.addEventListener('click', function () {
-									$('#confirmModal').modal('hide');
-								});
-							});
-						</script>
+							</script>
 						@else
 							<div class="alert alert-danger mt-3" role="alert">
 								Non hai nessun piatto!
