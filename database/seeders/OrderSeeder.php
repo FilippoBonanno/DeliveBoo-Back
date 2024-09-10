4986bbb7851bed695d6f6da873e75485adbbace0
MikeMac <micheledong2803@gmail.com>
2024-09-10 15:31:58 +0200

    transaction_id seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -20,6 +20,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'michele';
         $newOrder->buyer_address = 'via prova 1';
         $newOrder->buyer_phone = '+39 12341234';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -29,6 +30,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'luca';
         $newOrder->buyer_address = 'via esempio 2';
         $newOrder->buyer_phone = '+39 23452345';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -38,6 +40,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'giovanna';
         $newOrder->buyer_address = 'via test 3';
         $newOrder->buyer_phone = '+39 34563456';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -47,6 +50,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'alessandro';
         $newOrder->buyer_address = 'via campione 4';
         $newOrder->buyer_phone = '+39 45674567';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -56,6 +60,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'maria';
         $newOrder->buyer_address = 'via demo 5';
         $newOrder->buyer_phone = '+39 56785678';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -65,6 +70,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'roberto';
         $newOrder->buyer_address = 'via prova 6';
         $newOrder->buyer_phone = '+39 67896789';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -74,6 +80,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'francesca';
         $newOrder->buyer_address = 'via esempio 7';
         $newOrder->buyer_phone = '+39 78907890';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -83,6 +90,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'giorgio';
         $newOrder->buyer_address = 'via test 8';
         $newOrder->buyer_phone = '+39 89018901';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -92,6 +100,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'chiara';
         $newOrder->buyer_address = 'via campione 9';
         $newOrder->buyer_phone = '+39 90129012';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -101,6 +110,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'davide';
         $newOrder->buyer_address = 'via demo 10';
         $newOrder->buyer_phone = '+39 12341235';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -110,6 +120,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'eleonora';
         $newOrder->buyer_address = 'via prova 11';
         $newOrder->buyer_phone = '+39 23452346';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -119,6 +130,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'matteo';
         $newOrder->buyer_address = 'via esempio 12';
         $newOrder->buyer_phone = '+39 34563457';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -128,6 +140,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'valentina';
         $newOrder->buyer_address = 'via test 13';
         $newOrder->buyer_phone = '+39 45674568';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -137,6 +150,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'giulio';
         $newOrder->buyer_address = 'via campione 14';
         $newOrder->buyer_phone = '+39 56785679';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -146,6 +160,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'ilaria';
         $newOrder->buyer_address = 'via demo 15';
         $newOrder->buyer_phone = '+39 67896780';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -155,6 +170,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'lorenzo';
         $newOrder->buyer_address = 'via prova 16';
         $newOrder->buyer_phone = '+39 78907891';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -164,6 +180,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'federica';
         $newOrder->buyer_address = 'via esempio 17';
         $newOrder->buyer_phone = '+39 89018902';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -173,6 +190,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'riccardo';
         $newOrder->buyer_address = 'via test 18';
         $newOrder->buyer_phone = '+39 90129013';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -182,6 +200,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'sara';
         $newOrder->buyer_address = 'via campione 19';
         $newOrder->buyer_phone = '+39 12341236';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -191,6 +210,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'andrea';
         $newOrder->buyer_address = 'via demo 20';
         $newOrder->buyer_phone = '+39 23452347';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
 
         $newOrder = new Order();
@@ -200,6 +220,7 @@ class OrderSeeder extends Seeder
         $newOrder->buyer_name = 'antonella';
         $newOrder->buyer_address = 'via prova 21';
         $newOrder->buyer_phone = '+39 34563458';
+        $newOrder->transaction_id = 1;
         $newOrder->save();
     }
 }
