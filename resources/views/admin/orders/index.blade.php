90ed297e6fdac15b09684822250bd9d82a824310
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-10 16:14:58 +0200

    admin layout - Index Orders

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,7 +7,11 @@
 				<th scope="col">Id Transazione</th>
 				<th scope="col">Nome</th>
 				<th scope="col">Indirizzo</th>
+				<th scope="col">Codice Postale</th>
+				<th scope="col">Citta'</th>
+				<th scope="col">Provincia</th>
 				<th scope="col">N. Telefono</th>
+				<th scope="col">E-mail</th>
 				<th scope="col">Prezzo Totale</th>
 				<th scope="col">Data</th>
 			</tr>
@@ -18,7 +22,11 @@
 					<th scope="row">{{ $order->transaction_id }}</th>
 					<td>{{ $order->name }}</td>
 					<td>{{ $order->address }}</td>
+					<td> {{ $order->postalcode }}</td>
+					<td>{{ $order->city }}</td>
+					<td>{{ $order->province }}</td>
 					<td> {{ $order->phone }} </td>
+					<td> {{ $order->email }} </td>
 					<td>€{{ $order->total_price }}</td>
 					<td>{{ $order->created_at }}</td>
 				</tr>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,8 +20,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
 	<!-- jQuery and Bootstrap JS (Before app.js) -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 	<!-- Usando Vite -->
 	@vite(['resources/js/app.js'])
@@ -83,6 +83,15 @@
 									<a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dishes.create' ? 'bg-secondary' : '' }}"
 										href="{{ route('admin.dishes.create') }}">
 										<i class="fa-solid fa-square-plus fa-lg fa-fw"></i> Crea piatto
+									</a>
+								</li>
+							@endif
+
+							@if (Auth::user()->restaurant->orders)
+								<li class="nav-item">
+									<a class="nav-link text-white {{ Route::currentRouteName() == 'admin.orders' ? 'bg-secondary' : '' }}"
+										href="{{ route('admin.orders') }}">
+										<i class="fa-solid fa-list fa-lg fa-fw"></i> Lista Ordini Ricevuti
 									</a>
 								</li>
 							@endif
