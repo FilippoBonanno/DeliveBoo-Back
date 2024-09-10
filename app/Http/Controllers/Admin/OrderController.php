7ee11d28190838ae7cf716b6735aa3080793ae65
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-10 16:07:55 +0200

    restaurant model update - index order update - order controller index for auth users

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index()
     {
+        $user = auth()->user();
+        $orderList = $user->restaurant->orders()->orderByDesc('id')->get();
+
+        // $orderList = Order::orderByDesc('id')->get();
         $data = [
-            'orders' => 'ordini'
+            "orders" => $orderList,
         ];
+
         return view('admin.orders.index', $data);
     }
 }
