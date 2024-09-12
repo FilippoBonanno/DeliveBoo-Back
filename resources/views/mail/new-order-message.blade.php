cf32b92aeef95b321381410ab8e4feba60d0fb6f
MikeMac <micheledong2803@gmail.com>
2024-09-12 15:25:59 +0200

    email

--- resources/views/mail/new-order-message.blade.php
+++ resources/views/mail/new-order-message.blade.php
@@ -1,6 +1,21 @@
-<x-mail::message>
-    # {{ $lead->name }}
+@component('mail::message')
+# Grazie per il tuo ordine!
 
-    {{ $lead->email }}
+Ciao **{{ $lead->name }}**,
 
-</x-mail::message>
+Siamo felici di informarti che abbiamo ricevuto il tuo ordine! Ecco i dettagli del tuo ordine:
+
+Totale: **€{{ number_format($lead->total_price, 2) }}**
+
+## Cosa succede adesso?
+- Stiamo preparando il tuo ordine per la spedizione.
+- Riceverai un'altra email con i dettagli di tracciamento appena il tuo ordine sarà spedito.
+
+Se hai domande o desideri modificare il tuo ordine, non esitare a contattarci rispondendo a questa email o visitando la [nostra assistenza clienti](http://localhost:5174).
+
+Grazie ancora per aver scelto **{{ config('app.name') }}**.
+
+Saluti,<br>
+Il team di **{{ config('app.name') }}**
+
+@endcomponent
