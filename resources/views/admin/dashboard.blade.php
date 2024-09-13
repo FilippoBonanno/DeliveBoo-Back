eda6181295a16e74b555a66a9d9a013c15efeb4c
MikeMac <micheledong2803@gmail.com>
2024-09-13 17:10:01 +0200

    DashBoard grafica update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,62 +1,66 @@
 @extends('layouts.admin')
 
 @section('content')
-	<div class="container-fluid mt-4">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-					<div class="card-body">
-						@if (session('status'))
-							<div class="alert alert-success" role="alert">
-								{{ session('status') }}
-							</div>
-						@endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-						Sei Loggato! <br>
+                        @if (isset(Auth::user()->restaurant))
+                            <h1>{{ Auth::user()->restaurant->name }}</h1>
+                            <div class="card p-0 h-100">
 
-						@if (isset(Auth::user()->restaurant))
-							<h2>Il tuo ristorante :</h2>
-							<h1>{{ Auth::user()->restaurant->name }}</h1>
-							<div class="card p-0 h-100">
-								@if (Str::startsWith($restaurant->img, 'http'))
-									<div class="col-4">
-										<img src="{{ $restaurant->img }}" alt="immagine-ristorante" class="w-100 rounded-start">
+                                @if (Str::startsWith($restaurant->img, 'http'))
+                                    <div class="d-flex justify-content-center p-3">
+                                        <div class="col-xl-8 col-xs-12">
+                                            <img src="{{ $restaurant->img }}" alt="immagine-ristorante"
+                                                class="card-img">
+                                        </div>
 									</div>
-								@else
-									<div class="col-4">
-										<img src="{{ asset('storage/' . $restaurant->img) }}" class="w-100 rounded-start" alt="immagine-ristorante">
-									</div>
-								@endif
+                                @else
+									<div class="d-flex justify-content-center p-3">
+                                        <div class="col-xl-8 col-xs-12">
+                                            <img src="{{ asset('storage/' . $restaurant->img) }}" class="card-img"
+                                                alt="immagine-ristorante">
+                                        </div>
+                                    </div>
+                                @endif
 
-								<div class="row g-0 m-0">
-									<div class="col-12">
-										<div class="card-body">
-											<p class="card-text"><b>Nome Ristorante:</b> {{ $restaurant->name }}</p>
-											<p class="card-text"><b>Indirizzo Ristorante:</b> {{ $restaurant->address }}</p>
-											<p class="card-text"><b>Categorie:</b>
-												@foreach ($restaurant->categories as $category)
-													{{ $category->name }}
-													@if ($loop->index + 1 != count($restaurant->categories))
-														-
-													@endif
-												@endforeach
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
+                                <div class="row g-0 m-0">
+                                    <div class="col-12">
+                                        <div class="card-body">
+                                            <p class="card-text"><b>Nome Ristorante:</b> {{ $restaurant->name }}</p>
+                                            <p class="card-text"><b>Indirizzo Ristorante:</b> {{ $restaurant->address }}</p>
+                                            <p class="card-text"><b>Categorie:</b>
+                                                @foreach ($restaurant->categories as $category)
+                                                    {{ $category->name }}
+                                                    @if ($loop->index + 1 != count($restaurant->categories))
+                                                        -
+                                                    @endif
+                                                @endforeach
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-							@if(empty($dishes))
-							<div class="alert alert-danger mt-3" role="alert">
-								Non hai nessun piatto!
-							</div>
-							@endif
-						@endif
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            @if (empty($dishes))
+                                <div class="alert alert-danger mt-3" role="alert">
+                                    Non hai nessun piatto!
+                                </div>
+                            @endif
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
