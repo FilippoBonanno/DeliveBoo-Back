a52b9d55284c93e8360bf86930f6b1fb002a0943
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-13 17:20:47 +0200

    sintassi titolo

--- resources/views/admin/orders/chart.blade.php
+++ resources/views/admin/orders/chart.blade.php
@@ -5,67 +5,11 @@
 
 	{{-- Container for Chart --}}
 	<div class="container py-5 h-100">
-		<h2>Grafico Ordine - Mese</h2>
+		<h2>Grafico Ordini - Mese</h2>
 		<canvas id="ordersChart" width="400" height="200"></canvas>
 		<hr>
 		<h2>Grafico Profitto - Mese</h2>
 		<canvas id="incomeChart" width="400" height="200"></canvas>
-
-
-
-		{{-- <script>
-		const ctx = document.getElementById('combinedChart').getContext('2d');
-		const combinedChart = new Chart(ctx, {
-			type: 'bar', // Tipo principale di grafico
-			data: {
-				labels: {!! json_encode(array_keys($income)) !!}, // I mesi
-				datasets: [{
-						label: 'Introito totale in €',
-						data: {!! json_encode(array_values($income)) !!}, // Introito totale per mese
-						backgroundColor: 'rgba(75, 192, 192, 0.2)',
-						borderColor: 'rgba(75, 192, 192, 1)',
-						borderWidth: 1,
-						type: 'line', // Tipo di grafico per l'introito mensile
-						yAxisID: 'y1'
-					},
-					{
-						label: 'Numero di ordini',
-						data: {!! json_encode(array_values($orders)) !!}, // Numero ordini per mese
-						backgroundColor: 'rgba(153, 102, 255, 0.2)',
-						borderColor: 'rgba(153, 102, 255, 1)',
-						borderWidth: 1,
-						type: 'bar', // Tipo di grafico per il numero di ordini
-						yAxisID: 'y'
-					}
-				]
-			},
-			options: {
-				scales: {
-					y: {
-						beginAtZero: true,
-						ticks: {
-							// stepSize: 1,
-							// Includo il simbolo €
-							callback: function(value) {
-								return 'N.Ordini: ' + value;
-							}
-						}
-					},
-					y1: {
-						beginAtZero: true,
-						ticks: {
-							// Includo il simbolo €
-							callback: function(value) {
-								return '€' + value;
-							}
-						},
-						position: 'right',
-
-					}
-				}
-			}
-		});
-	</script> --}}
 
 		<script>
 			// Grafico degli ordini per mese
